14abf1b1d83a8be1ca86f191ae287b0751bffa7a
Alan Andrade <alan.andradec@gmail.com>
2017-12-06 14:59:38 -0800

    Carthage compatibility

---
+++ SVGKit-iOS-Framework/SVGKit-iOS-Framework.h
@@ -0,0 +1,18 @@
+//
+//  SVGKit-iOS-Framework.h
+//  SVGKit-iOS-Framework
+//
+//  Created by Alan Andrade on 12/6/17.
+//  Copyright © 2017 na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SVGKit/SVGKit.h
+
+//! Project version number for SVGKit-iOS-Framework.
+FOUNDATION_EXPORT double SVGKit_iOS_FrameworkVersionNumber;
+
+//! Project version string for SVGKit-iOS-Framework.
+FOUNDATION_EXPORT const unsigned char SVGKit_iOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGKit_iOS_Framework/PublicHeader.h>
